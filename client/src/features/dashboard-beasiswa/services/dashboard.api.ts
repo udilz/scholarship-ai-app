acfca60f2e92ebcbd732c21dd1b24df3f0fc8eee
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2024-10-10 22:17:50 +0700

    update fetch api

--- client/src/features/dashboard-beasiswa/services/dashboard.api.ts
+++ client/src/features/dashboard-beasiswa/services/dashboard.api.ts
@@ -2,7 +2,7 @@ import { IScholarship } from "../types/scholarship.type";
 
 export async function addBeasiswa({name, description, university, country, city, email, url_web, degrees, major, funding_type, open_date, close_date} : IScholarship) {
     try {
-        const res = await fetch('http://localhost:8000/api/v1/scholarships', {
+        const res = await fetch('http://localhost:8000/api/v1/scholarships/create/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             credentials: 'include',
@@ -24,7 +24,7 @@ export async function addBeasiswa({name, description, university, country, city,
 
 export async function getBeasiswa() {
     try {
-        const res = await fetch('http://localhost:8000/api/v1/scholarships')
+        const res = await fetch('http://localhost:8000/api/v1/scholarships/getall/')
 
         if (!res.ok) {
             throw new Error(`Server Error: ${res.status}`);
@@ -41,7 +41,7 @@ export async function getBeasiswa() {
 
 export async function getBeasiswaById(id:string) {
     try {
-        const res = await fetch(`http://localhost:8000/api/v1/scholarships/${id}`)
+        const res = await fetch(`http://localhost:8000/api/v1/scholarships/get/${id}`)
 
         if (!res.ok) {
             throw new Error(`Server Error: ${res.status}`);
@@ -79,7 +79,7 @@ export async function  deleteBeasiswa(id: string) {
 
 export async function updateBeasiswa(id: string, {name, description, university, country, city, email, url_web, degrees, major, funding_type, open_date, close_date} : IScholarship) {
     try {
-        const res = await fetch(`http://localhost:8000/api/v1/scholarships/${id}`, {
+        const res = await fetch(`http://localhost:8000/api/v1/scholarships/update/${id}`, {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             credentials: 'include',
